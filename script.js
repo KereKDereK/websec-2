41adad4428193e55213008bed2011f1064272839
KereK DereK <kerekderek1@gmail.com>
2022-10-24 01:23:34 +0400

    Main menu added

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ const menu = document.getElementById("menu");
 
 
 function mouseoverHandler(n){
-    console.log(menu.dataset.index)
     menu.dataset.index = n;
-    console.log(menu.dataset.index)
 }
