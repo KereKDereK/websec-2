dba815dc4caf26bf4926e8bf0fa78330ac250939
KereK DereK <kerekderek1@gmail.com>
2022-11-20 18:09:23 +0400

    pr issues fixed

--- client/multiplayer.js
+++ client/multiplayer.js
@@ -1,12 +1,12 @@
 function byField(field) {
     return (a, b) => a[field] > b[field] ? -1 : 1;
-  }
+}
 
 let button = document.querySelector('#button');
 let devError = document.querySelector('#error');
 let devLogin = document.querySelector('#devLogin');
-let leaderboard = document.querySelector('#leaderboard')
-let divs = document.getElementsByClassName('menu-item')
+let leaderboard = document.querySelector('#leaderboard');
+let divs = document.getElementsByClassName('menu-item');
 
 const widthCar = 25;
 const heightCar = 16;
@@ -16,123 +16,124 @@ const heightCoin = 25;
 
 let key = [0, 0, 0, 0];
 
-button.onclick = () =>{
+button.onclick = () => {
     let login = document.getElementById('login').value;
     socket.emit('login', login);
     console.log('send');
-    socket.on('login exist', ()=>{
+    socket.on('login exist', () => {
         devError.innerHTML = 'login exist';
         return;
     })
     devError.innerHTML = '';
-    socket.on("successful", ()=>{
+    socket.on("successful", () => {
         console.log("successful");
         devLogin.remove();
         document.body.appendChild(canvas);
-        leaderboard.style.display = 'block'
-        setInterval(()=>{socket.emit('key', key)}, 10);
+        leaderboard.style.display = 'block';
+        setInterval(() => { socket.emit('key', key) }, 10);
     })
 }
 
-let socket = io()
+let socket = io();
 
-const background = new Image(600,600);
+const background = new Image(600, 600);
 background.src = 'assets/game_background1.png';
 
 let canvas = document.createElement("canvas");
 let ctx = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height = 600;
-canvas.style.position = "absolute"
-canvas.style.top = "50%"
-canvas.style.left = "50%"
-canvas.style.marginRight = "-50%"
-canvas.style.transform = "translate(-50%, -50%)"
+canvas.style.position = "absolute";
+canvas.style.top = "50%";
+canvas.style.left = "50%";
+canvas.style.marginRight = "-50%";
+canvas.style.transform = "translate(-50%, -50%)";
 canvas.style.background = "white";
-canvas.style.margin = "0 auto"
+canvas.style.margin = "0 auto";
 
-const Car = new Image(25,16);
-Car.src = 'assets/car_1.png';
+const carImages = [];
+for (let i = 1; i < 11; i++) {
+    let car = new Image(25, 16);
+    car.src = `assets/car_${i}.png`;
+    carImages.push(car);
+}
 
-const coinImage = new Image(25,25);
+const coinImage = new Image(25, 25);
 coinImage.src = 'assets/coin.png';
 
 document.addEventListener('keydown', (event) => {
-  var name = event.key;
-  var code = event.code;
-  switch (name) {
-    case 'w':
-        key[0] = 1
-        break
-    case 'd':
-        key[1] = 1
-        break
-    case 'a':
-        key[2] = 1
-        break
-    case 's':
-        key[3] = 1
-        break
-    
+    var name = event.key;
+    var code = event.code;
+    switch (name) {
+        case 'w':
+            key[0] = 1;
+            break;
+        case 'd':
+            key[1] = 1;
+            break;
+        case 'a':
+            key[2] = 1;
+            break;
+        case 's':
+            key[3] = 1;
+            break;
     }
 }, false);
 
 
 document.addEventListener('keyup', (event) => {
-  var name = event.key;
-  var code = event.code;
-  switch (name) {
-    case 'w':
-        key[0] = 0
-        break
-    case 'd':
-        key[1] = 0
-        break
-    case 'a':
-        key[2] = 0
-        break
-    case 's':
-        key[3] = 0
-        break
+    var name = event.key;
+    var code = event.code;
+    switch (name) {
+        case 'w' || 'ц':
+            key[0] = 0;
+            break;
+        case 'd' || 'в':
+            key[1] = 0;
+            break;
+        case 'a' || 'ф':
+            key[2] = 0;
+            break;
+        case 's' || 'ы':
+            key[3] = 0;
+            break;
     }
 }, false);
 
 
 let draw = 1
 let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-socket.on('redraw', (data)=>{
-  ctx.drawImage(background, 0, 0);
-  const players = data[0]
-  draw = 1
-  arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  players.forEach(player => {
-    arr[draw-1] = {name: player.login, score: player.score}
-    Car.src = `assets/car_${draw}.png`;
-    ctx.save();
-    ctx.fillStyle = "rgb(255,255,255)";
-    ctx.translate(player.position.x, player.position.y)
-    ctx.rotate(0* Math.PI/180);
-    ctx.fillText(player.login, -15, -25, 60);
-    ctx.restore()
-    ctx.save()
-    ctx.fillStyle = 'red'
-    ctx.translate(player.position.x, player.position.y)
-    ctx.rotate(player.angle.alpha * (Math.PI/180))
-    ctx.drawImage(Car, 0, -8)
-    ctx.restore();
-    draw++;
-  });
-  arr.sort(byField('score'));
-  console.log(divs.length)
-  for (let i = 1; i <= divs.length-1; ++i){
+socket.on('redraw', (data) => {
+    ctx.drawImage(background, 0, 0);
+    const players = data[0];
+    draw = 1;
+    arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for (let player of players) {
+        arr[draw - 1] = { name: player.login, score: player.score };
+        ctx.save();
+        ctx.fillStyle = "rgb(255,255,255)";
+        ctx.translate(player.position.x, player.position.y)
+        ctx.rotate(0 * Math.PI / 180);
+        ctx.fillText(player.login, -15, -25, 60);
+        ctx.restore();
+        ctx.save();
+        ctx.fillStyle = 'red';
+        ctx.translate(player.position.x, player.position.y);
+        ctx.rotate(player.angle.alpha * (Math.PI / 180));
+        ctx.drawImage(carImages[draw - 1], 0, -8);
+        ctx.restore();
+        draw++;
+    }
+    arr.sort(byField('score'));
+    for (let i = 1; i <= divs.length - 1; ++i) {
         if (i > players.length) {
-            divs[i].style.display = 'none'
+            divs[i].style.display = 'none';
         }
-        else{
-            divs[i].style.display = 'block'
-            divs[i].innerHTML = `${i}. ${arr[i-1].name} ${arr[i-1].score}`
+        else {
+            divs[i].style.display = 'block';
+            divs[i].innerHTML = `${i}. ${arr[i - 1].name} ${arr[i - 1].score}`;
         }
-  }
-  const coin = data[1]
-  ctx.drawImage(coinImage, coin.x, coin.y, widthCoin, heightCoin);
+    }
+    const coin = data[1];
+    ctx.drawImage(coinImage, coin.x, coin.y, widthCoin, heightCoin);
 })
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'),
   app = express(),
   http = require('http').createServer(app),
-  io = require('socket.io')(http)
+  io = require('socket.io')(http);
 
 const host = 'localhost';
 const port = 8080;
@@ -13,201 +13,193 @@ let canvas = {
 
 let players = [];
 
-let coin = {x: getRandomInt(500),
-        y: getRandomInt(500)};
+let coin = {
+  x: getRandomInt(500),
+  y: getRandomInt(500)
+};
 
-setInterval(()=>{players.forEach(player => {
-  player.can_send = true;
-});}, 10);
+setInterval(() => {
+  players.forEach(player => {
+    player.can_send = true;
+  });
+}, 10);
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max + 50);
 }
 
 function point_rotation(x_old, y_old, player) {
-    let x = x_old * Math.cos(player.angle.alpha* (Math.PI/180)) - y_old * Math.sin(player.angle.alpha* (Math.PI/180))
-    let y = x_old * Math.sin(player.angle.alpha* (Math.PI/180)) + y_old * Math.cos(player.angle.alpha* (Math.PI/180))
-    return {x, y}
+  let x = x_old * Math.cos(player.angle.alpha * (Math.PI / 180)) - y_old * Math.sin(player.angle.alpha * (Math.PI / 180));
+  let y = x_old * Math.sin(player.angle.alpha * (Math.PI / 180)) + y_old * Math.cos(player.angle.alpha * (Math.PI / 180));
+  return { x, y };
 }
 
 function collision(player) {
 
-    let x = player.position.x
-    let y = player.position.y
+  let x = player.position.x;
+  let y = player.position.y;
 
-    let p1 = {x, y}
+  let p1 = { x, y };
 
-    let newx = 25
-    let newy = 16/2
+  let newx = 25;
+  let newy = 16 / 2;
 
-    let p2 = point_rotation(newx, newy, player)
+  let p2 = point_rotation(newx, newy, player);
 
-    p2.x += player.position.x
-    p2.y += player.position.y
+  p2.x += player.position.x;
+  p2.y += player.position.y;
 
-    newy = -16/2
+  newy = -16 / 2;
 
-    let p3 = point_rotation(newx, newy, player)
+  let p3 = point_rotation(newx, newy, player);
 
-    p3.x += player.position.x
-    p3.y += player.position.y
+  p3.x += player.position.x;
+  p3.y += player.position.y;
 
-    newx = 0
-    newy = 0
+  newx = 0;
+  newy = 0;
 
-    let p4 = point_rotation(newx, newy, player)
+  let p4 = point_rotation(newx, newy, player);
 
-    p4.x += player.position.x
-    p4.y += player.position.y
+  p4.x += player.position.x;
+  p4.y += player.position.y;
 
-    let points = [p1, p2, p3, p4]
-    return points
+  let points = [p1, p2, p3, p4];
+  return points;
 }
 
 io.on('connection', (socket) => {
-  console.log(`Client with id ${socket.id} connected`)
+  console.log(`Client with id ${socket.id} connected`);
 
   socket.on('login', (login) => {
     let loginExist = false;
-      players.forEach(player => {
-        if(player.login === login){
-            socket.emit('login exist');
-            loginExist = true;
-            return;
-        }
-      });
-      if(!loginExist){
-        players.push({
-          id: socket.id,
-          login: login,
-          can_send: true,
-          score: 0,
-          position: {
-            x: 20+400*Math.random(),
-            y: 20+400*Math.random()
-            },
-            velocity: {
-                m: 0,
-                x: 0,
-                y: 0
-            },
-          angle: {
-            alpha: 0
-            },
-          key: [0, 0, 0, 0],
-          points: []
-        });
-        socket.emit("successful");
+    players.forEach(player => {
+      if (player.login === login) {
+        socket.emit('login exist');
+        loginExist = true;
+        return;
       }
+    });
+    if (!loginExist) {
+      players.push({
+        id: socket.id,
+        login: login,
+        can_send: true,
+        score: 0,
+        position: {
+          x: 20 + 400 * Math.random(),
+          y: 20 + 400 * Math.random()
+        },
+        velocity: {
+          m: 0,
+          x: 0,
+          y: 0
+        },
+        angle: {
+          alpha: 0
+        },
+        key: [0, 0, 0, 0],
+        points: []
+      });
+      socket.emit("successful");
+    }
   })
 
-  socket.on('key', (keys)=>{
-    if(keys===undefined)
+  socket.on('key', (keys) => {
+    if (keys === undefined)
       return;
     let player = players.find((player) => player.id === socket.id);
 
-    if(player===undefined)
+    if (player === undefined)
       return;
-    if(player.can_send===false)
+    if (player.can_send === false)
       return;
     player.can_send = false;
 
 
     if (player.angle.alpha >= 361) {
-        player.angle.alpha = 1
+      player.angle.alpha = 1;
     }
     else if (player.angle.alpha <= -1) {
-        player.angle.alpha = 359
+      player.angle.alpha = 359;
     }
-    
 
-    if (keys[0] == 1){
-        player.velocity.m += 0.3
+
+    if (keys[0] == 1) {
+      player.velocity.m += 0.3;
     }
     else {
-        player.velocity.m *= 0.93
+      player.velocity.m *= 0.93;
     }
 
-    if (keys[3] == 1){
-        player.velocity.m -= 0.3
+    if (keys[3] == 1) {
+      player.velocity.m -= 0.3;
     }
 
     else {
-        player.velocity.m *= 0.93
+      player.velocity.m *= 0.93;
     }
 
-    if (keys[1] == 1){
-        player.angle.alpha += 4
+    if (keys[1] == 1) {
+      player.angle.alpha += 4;
     }
 
-    if (keys[2] == 1){
-        player.angle.alpha -= 4
+    if (keys[2] == 1) {
+      player.angle.alpha -= 4;
     }
 
-    player.points = collision(player)
-    
-    player.velocity.y = player.velocity.m * Math.abs(Math.cos((90-player.angle.alpha)*(Math.PI/180)))
-    player.velocity.x = player.velocity.m * Math.abs(Math.cos(player.angle.alpha*(Math.PI/180)))
+    player.points = collision(player);
 
-    player.x+=player.velocity * Math.sin(player.rotate * Math.PI / 180);
-    player.y-=player.velocity * Math.cos(player.rotate * Math.PI / 180);
+    player.velocity.y = player.velocity.m * Math.abs(Math.cos((90 - player.angle.alpha) * (Math.PI / 180)));
+    player.velocity.x = player.velocity.m * Math.abs(Math.cos(player.angle.alpha * (Math.PI / 180)));
+
+    player.x += player.velocity * Math.sin(player.rotate * Math.PI / 180);
+    player.y -= player.velocity * Math.cos(player.rotate * Math.PI / 180);
 
     for (let i = 0; i < player.points.length; ++i) {
-        if (player.points[i].x >= canvas.width - 8) {
-            player.velocity.m = 0
-            player.position.x = player.position.x - 2
-        }
-        else if (player.points[i].y >= canvas.height - 10) {
-            player.velocity.m = 0
-            player.position.y = player.position.y - 2
-        }
-        else if (player.points[i].x <= 12) {
-            player.velocity.m = 0
-            player.position.x = player.position.x + 2
-        }
-        else if (player.points[i].y <= 5) {
-            player.velocity.m = 0
-            player.position.y = player.position.y + 2
-        }
+      if (player.points[i].x >= canvas.width - 8) {
+        player.velocity.m = 0;
+        player.position.x = player.position.x - 2;
+      }
+      else if (player.points[i].y >= canvas.height - 10) {
+        player.velocity.m = 0;
+        player.position.y = player.position.y - 2;
+      }
+      else if (player.points[i].x <= 12) {
+        player.velocity.m = 0;
+        player.position.x = player.position.x + 2;
+      }
+      else if (player.points[i].y <= 5) {
+        player.velocity.m = 0;
+        player.position.y = player.position.y + 2;
+      }
     }
 
-    if(player.angle.alpha <= 90 && player.angle.alpha > 0){
-        player.position.y += player.velocity.y
-        player.position.x += player.velocity.x
+    if (player.angle.alpha <= 90 && player.angle.alpha > 0) {
+      player.position.y += player.velocity.y;
+      player.position.x += player.velocity.x;
     }
-    else if (player.angle.alpha <= 180 && player.angle.alpha > 90){
-        player.position.y += player.velocity.y
-        player.position.x -= player.velocity.x
+    else if (player.angle.alpha <= 180 && player.angle.alpha > 90) {
+      player.position.y += player.velocity.y;
+      player.position.x -= player.velocity.x;
     }
-    else if (player.angle.alpha <= 270 && player.angle.alpha > 180){
-        player.position.y -= player.velocity.y
-        player.position.x -= player.velocity.x
+    else if (player.angle.alpha <= 270 && player.angle.alpha > 180) {
+      player.position.y -= player.velocity.y;
+      player.position.x -= player.velocity.x;
     }
-    else if (player.angle.alpha <= 360 && player.angle.alpha > 270 || player.angle.alpha == 0){
-        player.position.y -= player.velocity.y
-        player.position.x += player.velocity.x
+    else if (player.angle.alpha <= 360 && player.angle.alpha > 270 || player.angle.alpha == 0) {
+      player.position.y -= player.velocity.y;
+      player.position.x += player.velocity.x;
     }
-
-    players.forEach(p => {
-      if(p!==player)
-        for (let i = 0; i < p.length; ++i){
-            if (p.points[i].x >= player.points[i].x - 3 && p.points[i].x <= player.points[i].x + 3 
-                && p.points[i].y >= player.points[i].y - 3 && p.points[i].y <= player.points[i].y + 3){
-                  console.log('working')
-                    p.velocity.m = -p.velocity.m
-                    player.velocity.m = -player.velocity.m
-            }
-        }
-    });
-
-    for (let i = 0; i < player.points.length; ++i){
-        if (player.points[i].x >= coin.x - 10 && player.points[i].x <= coin.x + 10 
-            && player.points[i].y >= coin.y - 10 && player.points[i].y <= coin.y + 10){
-                player.score++;
-                coin.x = getRandomInt(500);
-                coin.y = getRandomInt(500);
-                console.log(player.score);
-        }
+    
+    for (let i = 0; i < player.points.length; ++i) {
+      if (player.points[i].x >= coin.x - 3 && player.points[i].x <= coin.x + 28
+        && player.points[i].y >= coin.y - 3 && player.points[i].y <= coin.y + 28) {
+        player.score++;
+        coin.x = getRandomInt(500);
+        coin.y = getRandomInt(500);
+        console.log(player.score);
+      }
     }
 
     io.emit('redraw', [players, coin]);
@@ -215,27 +207,27 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     players.splice(players.find((player) => player.id === socket.id), 1)
-    console.log(`Client with id ${socket.id} disconnected`)
+    console.log(`Client with id ${socket.id} disconnected`);
   })
 })
 
-app.use(express.static(__dirname+'/client'))
+app.use(express.static(__dirname + '/client'));
 
-app.get('/', (req, res) =>{
-  res.sendFile(__dirname+'/client/index.html')
- })
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/index.html')
+});
 
 app.get('/players-count', (req, res) => {
   res.json({
     count: players.length,
   })
-})
+});
 
 app.get('/players', (req, res) => {
-    res.json({
-      players: players,
-    })
+  res.json({
+    players: players,
   })
+});
 
 app.post('/player/:id', (req, res) => {
   if (players.indexOf(req.params.id) !== -1) {
@@ -252,8 +244,8 @@ app.post('/player/:id', (req, res) => {
     return res
       .status(404)
       .json({ message: 'Client not found' })
-})
+});
 
 http.listen(port, host, () =>
   console.log(`Server listens http://${host}:${port}`)
-)
+);
